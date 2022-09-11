07a2b1f2f67c79006cd5bd3c1a1dc130c951e2aa
abbasoffanermin <105522485+abbasoffanermin@users.noreply.github.com>
2022-09-11 23:36:12 +0400

    Add files via upload

---
+++ 1/script.js
@@ -0,0 +1,11 @@
+let email= document.getElementById("email")
+let name= document.getElementById("name")
+function email(){
+email.value.toLowerCase()
+
+show.innerHTML =email
+}
+function name(){
+    name.value.ToUpperCase()
+show.innerHTML = name
+}
